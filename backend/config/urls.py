85c2c1c0ecafce25e4edca04a3cac75f55571f52
UmeshBabu7 <umeshtamang129@gmail.com>
2026-05-13 13:27:29 +0545

    chore(backend): initialize Django project with config and requirements

---
+++ backend/config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import api_urls
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(api_urls)),
+]
